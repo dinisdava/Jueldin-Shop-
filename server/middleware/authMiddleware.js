9d016ee5d7d74de1fe7202f90551afb3e7c51272
Dinis Dava <dinisdava29@gmail.com>
2025-05-05 09:12:13 +0200

    Create authMiddleware.js

---
+++ server/middleware/authMiddleware.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+
+const protect = async (req, res, next) => {
+  let token;
+
+  if (req.headers.authorization?.startsWith('Bearer')) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+
+  if (!token) {
+    return res.status(401).json({ message: 'Acesso não autorizado' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = await User.findById(decoded.id).select('-password');
+    next();
+  } catch (error) {
+    res.status(401).json({ message: 'Token inválido' });
+  }
+};
+
+module.exports = { protect };
