442914674a006f0733abbece8dbfd4479a83fb84
Dinis Dava <dinisdava29@gmail.com>
2025-05-05 09:24:35 +0200

    Create MpesaButton.js

---
+++ client/components/MpesaButton.js
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+
+export default function MpesaButton({ amount }) {
+  const [phone, setPhone] = useState('');
+
+  const handlePayment = async () => {
+    try {
+      const { data } = await axios.post('/api/mpesa/pay', { phone, amount });
+      alert('Pagamento iniciado! Confirme no seu celular');
+    } catch (error) {
+      alert('Erro: ' + error.response?.data?.message);
+    }
+  };
+
+  return (
+    <div className="mt-4">
+      <input
+        type="tel"
+        placeholder="84XXXXXXX"
+        value={phone}
+        onChange={(e) => setPhone(e.target.value)}
+        className="p-2 border rounded"
+      />
+      <button 
+        onClick={handlePayment}
+        className="bg-green-600 text-white p-2 ml-2 rounded"
+      >
+        Pagar com M-Pesa
+      </button>
+    </div>
+  );
+          }
