78d973f39848a00f51b95c1c3445e57f2e770ad8
Dinis Dava <dinisdava29@gmail.com>
2025-05-09 19:37:53 +0200

    Update next.config.js

--- client/next.config.js
+++ client/next.config.js
@@ -1,13 +1,20 @@
 module.exports = {
   async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://localhost:5000/api/:path*'
-      }
-    ];
+    return process.env.NODE_ENV === 'development' 
+      ? [
+          {
+            source: '/api/:path*',
+            destination: 'http://localhost:5000/api/:path*'
+          }
+        ]
+      : [];  // Em produção, API será acessada por URL absoluta
   },
   images: {
     domains: ['res.cloudinary.com'],
   },
+  env: {
+    API_URL: process.env.NODE_ENV === 'development' 
+      ? 'http://localhost:5000' 
+      : process.env.NEXT_PUBLIC_API_URL
+  }
 };
